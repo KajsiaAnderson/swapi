f449742b5e5909e2e92c2eea6c23b0f63830affe
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-10-19 23:28:25 -0600

    added get films button

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-let getResBtn = document.querySelector('button')
+let getResBtn = document.querySelector('.resident')
 
 let clickBtn = () => {
     console.log('button clicked')
     axios.get('https://swapi.dev/api/planets/?search=alderaan')
     .then((response) => {
+        console.log("response.data", response.data)
         const {residents} = response.data.results[0]
-        // console.log(residents)
+        console.log("response.data.results[0]", residents)
         residents.forEach(residentUrl => {
             axios.get(residentUrl).then((response) => {
                 console.log(response.data)
@@ -22,3 +23,29 @@ let clickBtn = () => {
 
 
 getResBtn.addEventListener('click', clickBtn)
+
+
+
+let getFilmBtn = document.querySelector('.films')
+
+let clickFilmBtn = () => {
+    // console.log("height button clicked")
+    axios.get('https://swapi.dev/api/planets/?search=alderaan')
+    .then((response) => {
+        console.log("response data", response.data)
+        const {films} = response.data.results[0]
+        console.log("response.data.results[0]", films)
+       films.forEach(filmsUrl => {
+        axios.get(filmsUrl)
+        .then((response) => {
+            console.log("films URL", response.data)
+            let newTitles = document.createElement('li')
+            newTitles.textContent=response.data.title
+            const movie = document.querySelector('.movie')
+            movie.appendChild(newTitles)
+        })
+       })
+    })
+}
+
+getFilmBtn.addEventListener('click', clickFilmBtn)
