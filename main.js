0e506548ef1db90f8078a5ade0ad9779e49b14e4
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-10-20 10:19:37 -0600

    add comments for title button

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let clickBtn = () => {
     .then((response) => {
         console.log("response.data", response.data)
         const {residents} = response.data.results[0]
+        // const resys = response.data.results[0].residents
         console.log("response.data.results[0]", residents)
         residents.forEach(residentUrl => {
             axios.get(residentUrl).then((response) => {
@@ -34,15 +35,24 @@ let clickFilmBtn = () => {
     .then((response) => {
         console.log("response data", response.data)
         const {films} = response.data.results[0]
+        // deconstructuring films. Getting the response data and key object results. At index 0.
         console.log("response.data.results[0]", films)
        films.forEach(filmsUrl => {
+        // filmsUrl is the element of the array. So each url of the film since films is an array.
         axios.get(filmsUrl)
+        // getting the films URL through axios
         .then((response) => {
+            // .then function after axios
             console.log("films URL", response.data)
+            // shows data from films URL
             let newTitles = document.createElement('li')
+            // create new list
             newTitles.textContent=response.data.title
+            // adding text of title for films in new list.
             const movie = document.querySelector('.movie')
+            // variable movie is now the movie class
             movie.appendChild(newTitles)
+            // the movie class has titles added
         })
        })
     })
